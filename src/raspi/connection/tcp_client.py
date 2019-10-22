ead72f5fc37261f6ed3337328ab559e86d91ad44
awojasinski <a.wojasinski@gmail.com>
2019-10-22 09:59:33 +0200

    script for tcp/ip connection

---
+++ src/raspi/connection/tcp_client.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = 'localhost'
+PORT = 10000
+MESSAGE = 'Hello world!'
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    print('Connecting to %s port %d' %(HOST, PORT))
+    s.connect((HOST, PORT))
+
+    try:
+
+        print('Sending:"%s"' %(MESSAGE))
+        s.sendall(MESSAGE.encode('utf-8'))
+
+        data = s.recv(1024)
+        print('Recived:"%s"' % (data.decode('utf-8')))
+
+    finally:
+
+        print('Closing socket')
+        s.close()
