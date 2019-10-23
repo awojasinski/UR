d435233ae59a7ad736637067bc02396bc16634f3
awojasinski <a.wojasinski@gmail.com>
2019-10-23 22:18:05 +0200

    live video from Raspberry Pi camera

---
+++ src/raspi/cvision/raspi_cam.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640,480))
+sleep(0.1)
+
+for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+
+	img = frame.array
+	
+	cv.imshow("Live", img)
+	key = cv.waitKey(1) & 0xFF
+	
+	rawCapture.truncate(0)
+	
+	if key == ord("q"):
+		break
+	
+
