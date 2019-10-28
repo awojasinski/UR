ecf1bd76d3dd73f8729d84fcf1177c62b02851c4
awojasinski <a.wojasinski@gmail.com>
2019-10-28 13:37:37 +0100

    improvements

--- src/raspi/cvision/qrcode_read.py
+++ src/raspi/cvision/qrcode_read.py
@@ -1,27 +1,28 @@
 import cv2 as cv
-import numpy as np
-import time
+from pyzbar import pyzbar
 
-def display(im, bbox):
-    n = len(bbox)
-    for j in range(n):
-        cv.line(im, tuple(bbox[j][0]), tuple(bbox[(j + 1) % n][0]), (255, 0, 0), 3)
+img = cv.imread('img/qr2.jpg')
 
-    # Display results
-    cv.imshow("Results", im)
+barcodes = pyzbar.decode(img)
+for barcode in barcodes:
+    # extract the bounding box location of the barcode and draw the
+    # bounding box surrounding the barcode on the image
+    (x, y, w, h) = barcode.rect
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-img = cv.imread('qr3.jpg')
-qrDecoder = cv.QRCodeDetector()
+    # the barcode data is a bytes object so if we want to draw it on
+    # our output image we need to convert it to a string first
+    barcodeData = barcode.data.decode("utf-8")
+    barcodeType = barcode.type
 
-data, bbox, rectifiedImg = qrDecoder.detectAndDecode(img)
-if len(data)>0:
-    print("Decoded Data : {}".format(data))
-    display(img, bbox)
-    rectifiedImage = np.uint8(rectifiedImg);
-    cv.imshow("Rectified QRCode", rectifiedImg);
-else:
-    print("QR Code not detected")
-    cv.imshow("Results", img)
+    # draw the barcode data and barcode type on the image
+    text = "{} ({})".format(barcodeData, barcodeType)
+    cv.putText(img, text, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 0, 255), 2)
 
+    # print the barcode type and data to the terminal
+    print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+
+# show the output image
+cv.imshow("Image", img)
 cv.waitKey(0)
-cv.destroyAllWindows()
