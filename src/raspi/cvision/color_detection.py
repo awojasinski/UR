8a1d9bb8c8ba8e6d1be5c9de6195bd8293d37cf3
awojasinski <a.wojasinski@gmail.com>
2019-10-29 13:14:51 +0100

    bug fixed

--- src/raspi/cvision/color_detection.py
+++ src/raspi/cvision/color_detection.py
@@ -19,13 +19,15 @@ import numpy as np
 #}
 
 img = cv2.imread('img/shapes_and_colors.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 blurred = cv2.GaussianBlur(gray, (5,5), 0)
 thresh = cv2.threshold(blurred, 70, 255, cv2.THRESH_BINARY)[1]
 
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
+
+pos = np.zeros([len(cnts), 2], dtype='uint8')
 
 i = 0
 for c in cnts:
@@ -38,6 +40,9 @@ for c in cnts:
 
     cv2.drawContours(img, [c], -1, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.circle(img, (cX, cY), 4, (255, 255, 255), -1)
+    
+for i in range(len(cnts)):
+    print(img[pos.item((i, 0)), pos.item((i, 1))])
 
 cv2.imshow("Image thresh", img)
 cv2.waitKey(0)
