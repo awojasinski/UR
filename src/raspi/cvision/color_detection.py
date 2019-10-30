9f58e24d1cf30439ef559bc304c29a268fb80d40
awojasinski <a.wojasinski@gmail.com>
2019-10-30 13:09:43 +0100

    some improvements

--- src/raspi/cvision/color_detection.py
+++ src/raspi/cvision/color_detection.py
@@ -1,49 +1,41 @@
 # TODO:
-#   - define ranges of specific colors (dictionary)
 #   - ranges of colors in separated in different file
-#   - create function colorDetect(img, cX, cY) return string color
+#   - create function colorDetect(rgb_pixel_value) return string color
 
-import cv2
+import cv2 as cv
 import imutils
 import numpy as np
 
-#colors = {
-    #'red' : np.array((), ()),
-    #'blue' : np.array((), ()),
-    #'orange' : np.array((), ()),
-    #'pink' : np.array((), ()),
-    #'white' : np.array((), ()),
-    #'green' : np.array((), ()),
-    #'yellow' : np.array((), ()),
-    #'violet' : np.array((), ()),
-#}
+colors = {
+    'red': np.array([255, 100, 100]),
+    'blue': np.array([100, 100, 255]),
+    'orange': np.array([255, 183, 101]),
+    'magenta': np.array([255, 100, 255]),
+    'grey': np.array([178, 178, 178]),
+    'green': np.array([100, 255, 100]),
+    'yellow': np.array([255, 255, 100]),
+    'cyan': np.array([192, 255, 255]),
+}
 
-img = cv2.imread('img/shapes_and_colors.jpg')
-#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-blurred = cv2.GaussianBlur(gray, (5,5), 0)
-thresh = cv2.threshold(blurred, 70, 255, cv2.THRESH_BINARY)[1]
+img = cv.imread('img/shapes_and_colors.jpg')
+gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+blurred = cv.GaussianBlur(gray, (5, 5), 0)
+thresh = cv.threshold(blurred, 70, 255, cv.THRESH_BINARY)[1]
 
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 
-pos = np.zeros([len(cnts), 2], dtype='uint8')
+points = np.empty(shape = (0,3))
 
-i = 0
 for c in cnts:
-    M = cv2.moments(c)
+    M = cv.moments(c)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
-    pos[i][0] = cX
-    pos[i][1] = cY
-    i = i + 1
+    points = np.append(points, [[cX, cY, img[cX, cY]]], axis=0)
 
-    cv2.drawContours(img, [c], -1, (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.circle(img, (cX, cY), 4, (255, 255, 255), -1)
-    
-for i in range(len(cnts)):
-    print(img[pos.item((i, 0)), pos.item((i, 1))])
+    cv.drawContours(img, [c], -1, (0, 0, 255), 2, cv.LINE_AA)
+    cv.circle(img, (cX, cY), 4, (255, 255, 255), -1)
 
-cv2.imshow("Image thresh", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv.imshow("Image thresh", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
