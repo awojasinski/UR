cff30019653ace7775f4c2b0462d9ab83b2eedc5
awojasinski <a.wojasinski@gmail.com>
2019-10-30 17:31:23 +0100

    add new dictionary to search color of the pixel

--- src/raspi/cvision/color_detection.py
+++ src/raspi/cvision/color_detection.py
@@ -1,21 +1,31 @@
-# TODO:
-#   - ranges of colors in separated in different file
+# TODO
+#   - color2=Noneges of colors in separated in different file
 #   - create function colorDetect(rgb_pixel_value) return string color
 
 import cv2 as cv
 import imutils
 import numpy as np
+import math
+from matplotlib import pyplot as plt
 
 colors = {
-    'red': np.array([255, 100, 100]),
-    'blue': np.array([100, 100, 255]),
-    'orange': np.array([255, 183, 101]),
-    'magenta': np.array([255, 100, 255]),
-    'grey': np.array([178, 178, 178]),
-    'green': np.array([100, 255, 100]),
-    'yellow': np.array([255, 255, 100]),
-    'cyan': np.array([192, 255, 255]),
+    'red': np.array([85, 0, 0]),
+    'blue': np.array([0, 0, 85]),
+    'orange': np.array([255, 128, 0]),
+    'magenta': np.array([255, 0, 255]),
+    'green': np.array([0, 85, 0]),
+    'yellow': np.array([170, 170, 0]),
+    'cyan': np.array([0, 255, 255]),
 }
+
+def colorRecognition(pixelRGB):
+    newDict = dict()
+    for (name, rbg) in colors.items():
+        if pixelRGB[0] > rbg[0] and pixelRGB[1] > rbg[1] and pixelRGB[2] > rbg[2]:
+            newDict[name] = rbg
+    print(newDict)
+    return
+
 
 img = cv.imread('img/shapes_and_colors.jpg')
 gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
@@ -25,7 +35,7 @@ thresh = cv.threshold(blurred, 70, 255, cv.THRESH_BINARY)[1]
 cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 
-points = np.empty(shape = (0,3))
+points = np.empty(shape=(0, 3))
 
 for c in cnts:
     M = cv.moments(c)
@@ -36,6 +46,14 @@ for c in cnts:
     cv.drawContours(img, [c], -1, (0, 0, 255), 2, cv.LINE_AA)
     cv.circle(img, (cX, cY), 4, (255, 255, 255), -1)
 
-cv.imshow("Image thresh", img)
+#for c in range(len(points)):
+ #   cv.putText(img, str(c+1)+'.'+points[c][3], tuple([points[c][0], points[c][1]]), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
+
+for i in range(len(points)):
+    print(str(points[i]))
+    colorRecognition(points[i][2])
+
+plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
+plt.show()
 cv.waitKey(0)
 cv.destroyAllWindows()
