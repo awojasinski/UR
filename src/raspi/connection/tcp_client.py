a8f4c1c900cfd21819defad5021a3c8c7114be67
awojasinski <a.wojasinski@gmail.com>
2019-11-21 10:07:46 +0100

    helllo

--- src/raspi/connection/tcp_client.py
+++ src/raspi/connection/tcp_client.py
@@ -1,23 +1,17 @@
 import socket
 
-HOST = '192.168.1.113'
-PORT = 10000
-MESSAGE = 'Hello world!'
+HOST = "192.168.4.1"
+PORT = 23
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
-    print('Connecting to %s port %d' %(HOST, PORT))
+    print('Connecting to %s port %d' % (HOST, PORT))
     s.connect((HOST, PORT))
-
-    try:
-
-        print('Sending:"%s"' %(MESSAGE))
-        s.sendall(MESSAGE.encode('utf-8'))
-
+    message = input('-> ')
+    while message != 'q':
+        print('Sending:"%s"' % (message))
+        s.send(message.encode('utf-8'))
         data = s.recv(1024)
         print('Recived:"%s"' % (data.decode('utf-8')))
-
-    finally:
-
-        print('Closing socket')
-        s.close()
+        message = input('-> ')
+    print('Closing socket')
+    s.close()
