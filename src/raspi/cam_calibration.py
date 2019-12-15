03cc119d2ca0acfd16e87101468cc2e65a667554
awojasinski <a.wojasinski@gmail.com>
2019-12-15 20:03:38 +0100

    added writing mtx and dist to config.json

--- src/raspi/cam_calibration.py
+++ src/raspi/cam_calibration.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import glob
+import json
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -13,7 +14,7 @@ objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('cam_correction_photos/*.png')
+images = glob.glob('cam_correction_photos\\*.png')
 
 for fname in images:
     img = cv2.imread(fname)
@@ -29,12 +30,51 @@ for fname in images:
         corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners)
 
-        # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
-        cv2.imshow('img', img)
-        cv2.imwrite('cam_correction_photos/correction/'+fname[22:], img)
-        cv2.waitKey(500)
-
-cv2.destroyAllWindows()
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+config['cam_calibration']['mtx'] = mtx.tolist()
+config['cam_calibration']['dist'] = dist.tolist()
+
+with open('config.json', 'w') as config_file:
+    json.dump(config, config_file, sort_keys=True, indent=4)
+
+
+#print(ret)
+#print(mtx)
+#print(dist)
+
+images = glob.glob('cam_correction_photos\\*.png')
+
+for fname in images:
+    img_org = cv2.imread(fname)
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('original img', img_org)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv2.imshow('img with chessboard corners', img)
+
+    img_dist = cv2.imread(fname)
+    h, w = img_dist.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+    # undistort
+    dst = cv2.undistort(img_dist, mtx, dist, None, newcameramtx)
+
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y + h, x:x + w]
+
+    cv2.imshow('undistored img', dst)
+    cv2.waitKey(1000)
