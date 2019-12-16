c7cdb8582e10714ac7ad54b802b802cf7b38c6a6
awojasinski <a.wojasinski@gmail.com>
2019-12-16 11:21:38 +0100

    added new module for transformation between camera and robot coordinates

--- src/raspi/cvision/__init__.py
+++ src/raspi/cvision/__init__.py
@@ -2,3 +2,4 @@ from cvision.object_detection import *
 from cvision.config_read import *
 from cvision.find_element import *
 from cvision.draw_element import *
+from cvision.coordinates_transformation import *
