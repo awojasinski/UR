c8c0f167189404f2c321c2227d7a5282ee2ba55a
awojasinski <a.wojasinski@gmail.com>
2019-12-26 14:24:54 +0100

    Script for automated photo capturing

---
+++ src/RaspberryPi/cameraChessboardAuto.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from time import sleep
+
+# Początkowe ustawienia kamery
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# Zatrzymanie programu aby kamera mogła się uruchomić
+sleep(0.1)
+
+i = 0   # Zmienna do numerowania uchwyconych zdjęć szachownicy
+
+for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+
+    # Trójwymiarowa macierz o wymiarach szerokość, wysokość i kanał koloru
+    img = frame.array   # Zapisanie akutalnego kadru do zmiennej
+    # Dwuwymiarowa macierz szerokość na wysokość wartości od 0 do 255
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Zamiana przestrzeni barw na skale szarości
+
+    cv.imshow('Camera', img)    # Wyświetlenie okna z podglądem obrazu
+
+    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)     # Szukanie szachownicy
+
+    if ret:  # Jeśli wykryto w kadrze szachownicę
+        print('Wykonano zdjęcie nr:', i)    # Wyświetlenie informacji o wykonaniu zdjęcia
+        cv.imwrite('cam_correction_photos\\'+str(i)+'.png')     # Zapisanie zdjęcia do folderu
+        i += 1      # Inkrementacja numeracji zdjęć
+
+    # Oczekiwanie na wciśnięcie klawisza przez użytkownika, klatka wyświetlana jest przez 1ms
+    # Operator logiczny AND sprawia że ważny jest tylko pierwszy bajt zwracany przez funkcję
+    # dzięki temu nie ma znaczenia czy klwaisz został wciśnięty z włączonym CapsLock czy nie
+    key = cv.waitKey(1) & 0xFF
+
+    if key == ord('q'):     # Jeśli wciśnięty klawisz to 'q' zamknij okno i zakończ program
+        cv.destroyAllWindows()
+        break
+
+    rawCapture.truncate(0)  # Wyczyszczenie strumienia, aby przygotować go na kolejną klatkę
