0360cedcad17266928f0bd72d48a708bf5862d6e
awojasinski <a.wojasinski@gmail.com>
2019-12-29 13:37:47 +0100

    fixed to work properly on win and linux

--- src/RaspberryPi/cvision/test.py
+++ src/RaspberryPi/cvision/test.py
@@ -1,9 +1,15 @@
 import os
-
-path = os.getcwd()
-path = path.split('/')
-path.pop()
 dir = ""
-for p in path:
-    dir = dir + p + '/'
+path = os.getcwd()
+print(path)
+if '/' in path:
+    path = path.split('/')
+    path.pop()
+    for p in path:
+        dir = dir + p + '/'
+if '\\' in path:
+    path = path.split('\\')
+    path.pop()
+    for p in path:
+        dir = dir + p + '\\'
 print(dir)
